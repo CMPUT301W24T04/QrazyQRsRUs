65937c1fc46d898d6219eef1595165eafc0b9bba
Afraz Khan <afrazk21@gmail.com>
2024-02-20 01:18:23 -0700

    added Event class | .idea folder added to gitignore

---
+++ code/app/src/main/java/com/example/qrazyqrsrus/Event.java
@@ -0,0 +1,56 @@
+package com.example.qrazyqrsrus;
+
+import java.util.Date;
+
+
+public class Event {
+    private String eventName;
+    private String location;
+    private Date date;
+    private String details;
+
+    // Default constructor
+    public Event() {
+    }
+
+    public Event(String eventName, String location, Date date, String details) {
+        this.eventName = eventName;
+        this.location = location;
+        this.date = date;
+        this.details = details;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+}
+
