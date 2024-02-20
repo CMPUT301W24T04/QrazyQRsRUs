49632d3af771ffccfb4f158ed6107fc139f83c53
john00003 <jetyler@ualberta.ca>
2024-02-19 19:22:17 -0700

    started xml files for the fragments of a new event creation sequence. made xml file for nav host. got navhost and navcontroller in main activity

--- code/app/src/main/java/com/example/qrazyqrsrus/MainActivity.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.qrazyqrsrus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private NavHostFragment navHostFragment;
+//    private NavController navController = Navigation.findNavController(this, R.id.new_event_nav_host);
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.new_event_nav_host);
+        navController = navHostFragment.getNavController();
     }
 }
