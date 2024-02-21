32da87a28c55e8b3c92f2542b7b4b007984b927a
john00003 <jetyler@ualberta.ca>
2024-02-21 07:39:52 -0700

    updated fragment xml files to have a toolbar and fragments to have a toolbar, set menuOnClickListeners to navigate to the previous new event screen or leave altogether. updated navGraph. updated themes to have no toolbar by default.

--- code/app/src/main/java/com/example/qrazyqrsrus/NewEventImageFragment.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/NewEventImageFragment.java
@@ -2,12 +2,19 @@ package com.example.qrazyqrsrus;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-public class NewEventImageFragment extends Fragment {
+public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuItemClickListener {
+    private Toolbar toolbar;
     public static NewEventImageFragment newInstance(String param1, String param2) {
         NewEventImageFragment fragment = new NewEventImageFragment();
         Bundle args = new Bundle();
@@ -21,12 +28,47 @@ public class NewEventImageFragment extends Fragment {
         if (getArguments() != null) {
 
         }
+        //alternative method, giving toolbar to activity rather than fragment
+//        toolbar = (Toolbar) getActivity().findViewById(R.id.image_screen_toolbar);
+        //we must cast the fragment's activity to an AppCompatActivity in order to invoke setSupportActionBar
+        //this idea was found from https://stackoverflow.com/questions/29020935/using-toolbar-with-fragments on February 21st, 2024
+        //it was posted by user vinitius (https://stackoverflow.com/users/3112331/vinitius) in the reply https://stackoverflow.com/a/29021287
+//        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.new_event_image_fragment, container, false);
+        View returnView = inflater.inflate(R.layout.new_event_image_fragment, container, false);
+        createToolbar(returnView);
+        return returnView;
+    }
+
+    private void createToolbar(View view){
+        //once we have made the view, we create the toolbar and inflate it's menu, in order to set and onclicklistener from the fragment
+        //the idea to access the toolbar by using the Fragment's host View was taken from https://stackoverflow.com/questions/29020935/using-toolbar-with-fragments on February 21st, 2024
+        //it was posted by the user Faisal Naseer (https://stackoverflow.com/users/2641848/faisal-naseer) in the post https://stackoverflow.com/a/45653449
+        toolbar = (Toolbar) view.findViewById(R.id.image_screen_toolbar);
+        toolbar.inflateMenu(R.menu.menu_with_back_button);
+        //the fragment implements the Toolbar.OnMenuItemClick interface, pass itself.
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        int id = item.getItemId();
+        //we check which item id was clicked on, navigating accordingly
+        if (id == R.id.back_button){
+            //go to previous screen
+            Navigation.findNavController(getView()).navigate(R.id.action_newEventImageFragment_to_newEventTextFragment);
+            return true;
+        }
+        else if (id == R.id.cancel_button){
+            //leave entire new event sequence
+            Navigation.findNavController(getView()).navigate(R.id.action_newEventImageFragment_to_newEventFragment);
+            return true;
+        }
+        return false;
     }
 }
--- code/app/src/main/java/com/example/qrazyqrsrus/NewEventTextFragment.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/NewEventTextFragment.java
@@ -2,15 +2,18 @@ package com.example.qrazyqrsrus;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class NewEventTextFragment extends Fragment {
+public class NewEventTextFragment extends Fragment implements Toolbar.OnMenuItemClickListener{
+    private Toolbar toolbar;
     public static NewEventTextFragment newInstance(String param1, String param2) {
         NewEventTextFragment fragment = new NewEventTextFragment();
         Bundle args = new Bundle();
@@ -35,7 +38,27 @@ public class NewEventTextFragment extends Fragment {
         fab.setOnClickListener(v -> {
             Navigation.findNavController(view).navigate(R.id.action_newEventTextFragment_to_newEventImageFragment2);
         });
+        createToolbar(view);
         return view;
+    }
+    private void createToolbar(View view){
+        //once we have made the view, we create the toolbar and inflate it's menu, in order to set and onclicklistener from the fragment
+        //the idea to access the toolbar by using the Fragment's host View was taken from https://stackoverflow.com/questions/29020935/using-toolbar-with-fragments on February 21st, 2024
+        //it was posted by the user Faisal Naseer (https://stackoverflow.com/users/2641848/faisal-naseer) in the post https://stackoverflow.com/a/45653449
+        toolbar = (Toolbar) view.findViewById(R.id.text_screen_toolbar);
+        toolbar.inflateMenu(R.menu.menu_no_back_button);
+        //the fragment implements the Toolbar.OnMenuItemClick interface, pass itself.
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.cancel_button){
+            Navigation.findNavController(getView()).navigate(R.id.action_newEventTextFragment_to_newEventFragment);
+            return true;
+        }
+        return false;
     }
 
 }
