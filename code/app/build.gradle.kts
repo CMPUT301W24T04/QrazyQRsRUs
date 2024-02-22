4083156b6056634751388383a962ca48d5e12393
Mikale-UofA <lnineza@ualberta.ca>
2024-02-21 19:40:07 -0700

    Fixed gradle issues

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-firestore")
 
 }
