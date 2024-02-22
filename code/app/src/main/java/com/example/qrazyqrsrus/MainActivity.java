39fec443bab66718bd46a2dc5617ec1873da6bc6
Mikale-UofA <lnineza@ualberta.ca>
2024-02-21 19:04:15 -0700

    Made the navigation bar

--- code/app/src/main/java/com/example/qrazyqrsrus/MainActivity.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/MainActivity.java
@@ -1,15 +1,48 @@
 package com.example.qrazyqrsrus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.qrazyqrsrus.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ChangeFragment(new HomeFragment());
 
+
+        binding.BottomNavView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home){
+                ChangeFragment(new HomeFragment());
+            } else if (id == R.id.scan) {
+                // TO DO
+            } else if (id == R.id.my_events) {
+                ChangeFragment(new MyEventsFragment());
+            } else if (id == R.id.profile) {
+                ChangeFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+
+    }
+
+    private void ChangeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
