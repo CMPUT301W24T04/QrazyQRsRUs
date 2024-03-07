9701fdcc21b03e9cb8f1aec0bee05f54c3674817
Aditya Shahi <ashahi1@ualberta.com>
2024-03-06 20:35:54 -0700

    Remove edit profile button from MainActivity

--- code/app/src/main/java/com/example/qrazyqrsrus/MainActivity.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/MainActivity.java
@@ -1,14 +1,7 @@
 package com.example.qrazyqrsrus;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,17 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the button by its ID
-        Button btnEditProfile = findViewById(R.id.btnEditProfile);
 
-        // Set an OnClickListener on the button
-        btnEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an Intent to start UpdateProfileActivity
-                Intent intent = new Intent(MainActivity.this, UpdateProfileActivity.class);
-                startActivity(intent); // Start the new activity
-            }
-        });
     }
 }
