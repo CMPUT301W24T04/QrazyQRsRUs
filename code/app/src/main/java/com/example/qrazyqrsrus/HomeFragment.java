efe9541cd47337121ea97f580486eaa681dcce46
Mikale-UofA <lnineza@ualberta.ca>
2024-03-07 21:44:12 -0700

    Fixed stuff

--- code/app/src/main/java/com/example/qrazyqrsrus/HomeFragment.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/HomeFragment.java
@@ -65,6 +65,7 @@ public class HomeFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Bundle args = new Bundle();
                 args.putSerializable("event", checkedInEvents.get(i));
+                args.putSerializable("attendee", attendee);
                 Navigation.findNavController(view).navigate(R.id.action_mainMenu_to_eventDetailsFragment, args);
             }
         });
