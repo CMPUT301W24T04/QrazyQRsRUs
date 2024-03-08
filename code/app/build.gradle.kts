87f13ec806ca1854740770af754cc8b0f1a64e63
Aditya Shahi <ashahi1@ualberta.com>
2024-03-07 21:06:51 -0700

    created test cases for model classes.

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:4.11.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //Navigation
@@ -49,8 +50,10 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:$nav_version")
     implementation("androidx.navigation:navigation-ui:$nav_version")
 
+
     // Feature module Support
     implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
 
     // Testing Navigation
     androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
@@ -61,12 +64,24 @@ dependencies {
     //ZXing QR Codes
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 
+
     //firestore
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
 
+
     //photo picker
     implementation("androidx.activity:activity:1.8.2")
+
+
+    // Mockito for unit tests
+    testImplementation("org.mockito:mockito-core:4.11.0")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Add the Google Play Services Tasks API for Tasks.forResult
+    implementation("com.google.android.gms:play-services-tasks:18.1.0")
+
 }
 
