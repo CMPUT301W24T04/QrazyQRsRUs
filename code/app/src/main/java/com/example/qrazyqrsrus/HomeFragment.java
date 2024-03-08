a1baa921300796944bf1c39c2385084e70d46569
Mikale-UofA <lnineza@ualberta.ca>
2024-03-07 21:27:48 -0700

    Made the fragment for the events user has checked in or signed up to and set up the navigation between that and the event detail

--- code/app/src/main/java/com/example/qrazyqrsrus/HomeFragment.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/HomeFragment.java
@@ -3,10 +3,12 @@ package com.example.qrazyqrsrus;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,6 +17,8 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment} factory method to
  * create an instance of this fragment.
+ * It is meant to create a fragment that displays the events
+ * the user has either signed up to, or checked into.
  */
 
 public class HomeFragment extends Fragment {
@@ -40,6 +44,7 @@ public class HomeFragment extends Fragment {
             System.out.println("No Arguments provided");
             return null;
         }
+
         ListView checkedIn = view.findViewById(R.id.checked_in_events_listview);
         ListView signedUp = view.findViewById(R.id.signed_up_events_listview);
 
@@ -54,6 +59,24 @@ public class HomeFragment extends Fragment {
         HomeSignedUpListAdapter homeSignedUpListAdapter = new HomeSignedUpListAdapter(getContext(), signedUpEvents);
         checkedIn.setAdapter(homeCheckedInListAdapter);
         signedUp.setAdapter(homeSignedUpListAdapter);
+
+        checkedIn.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Bundle args = new Bundle();
+                args.putSerializable("event", checkedInEvents.get(i));
+                Navigation.findNavController(view).navigate(R.id.action_mainMenu_to_eventDetailsFragment, args);
+            }
+        });
+
+        signedUp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Bundle args = new Bundle();
+                args.putSerializable("event", signedUpEvents.get(i));
+                Navigation.findNavController(view).navigate(R.id.action_mainMenu_to_eventDetailsFragment, args);
+            }
+        });
 
 
         return view;
