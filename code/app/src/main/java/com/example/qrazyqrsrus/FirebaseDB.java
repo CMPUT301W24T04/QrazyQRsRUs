9f05f25f6a824ba31d21c86292e0bcbc58156ab6
arber <arber@ualberta.ca>
2024-03-29 09:42:52 -0600

    Uncommented if-statement in firebaseDB.

--- code/app/src/main/java/com/example/qrazyqrsrus/FirebaseDB.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/FirebaseDB.java
@@ -1295,9 +1295,9 @@ public class FirebaseDB {
                                 // long
                                 long checkins = (long) document.getData().get("numberOfCheckIns"); // changed to type long
                                 Attendee attendee = new Attendee(id, documentId, name, email, profilePicturePath, geolocationOn, checkins);
-//                                if(attendee.getGeolocationOn() == true) {
+                                if(attendee.getGeolocationOn() == true) {
                                     attendeeDataList.add(attendee);
-//                                }
+                                }
                             }
                         }
                     }
