a9aefa3a8f8d21e9517f443ea2a8df2ebb2218e1
Aditya Shahi <ashahi1@ualberta.ca>
2024-04-06 22:56:34 -0600

    MyEventListFragment

--- code/app/src/androidTest/java/com/example/qrazyqrsrus/MyEventsListFragmentTest.java
+++ code/app/src/androidTest/java/com/example/qrazyqrsrus/MyEventsListFragmentTest.java
@@ -1,4 +1,12 @@
+package com.example.qrazyqrsrus;
+
 import static org.junit.Assert.*;
+
+import org.junit.Before;
+
 public class MyEventsListFragmentTest {
-  
+
+    @Before
+    public void setUp() throws Exception {
+    }
 }
