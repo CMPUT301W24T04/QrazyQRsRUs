acf70bc54ca5ec098f949b3553b437b3683e494d
Afraz Khan <afrazk21@gmail.com>
2024-04-08 13:57:34 -0600

    ...

--- code/app/src/main/java/com/example/qrazyqrsrus/NewEventImageFragment.java
+++ code/app/src/main/java/com/example/qrazyqrsrus/NewEventImageFragment.java
@@ -150,6 +150,12 @@ public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuIte
         return view;
     }
 
+    /**
+     * Initializes and sets up the toolbar for the fragment. This method inflates the toolbar menu and sets a listener for menu item clicks.
+     * The toolbar provides navigation options such as going back to the previous screen or canceling the event creation process.
+     *
+     * @param view The root view of the fragment where the toolbar is located.
+     */
     private void createToolbar(View view){
         //once we have made the view, we create the toolbar and inflate it's menu, in order to set and onclicklistener from the fragment
         //the idea to access the toolbar by using the Fragment's host View was taken from https://stackoverflow.com/questions/29020935/using-toolbar-with-fragments on February 21st, 2024
@@ -160,6 +166,13 @@ public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuIte
         toolbar.setOnMenuItemClickListener(this);
     }
 
+    /**
+     * Handles menu item clicks in the toolbar. Depending on the item clicked, this method navigates to the previous fragment in the event creation sequence,
+     * or exits the event creation flow altogether.
+     *
+     * @param item The menu item that was clicked.
+     * @return true if the menu item click was handled, false otherwise.
+     */
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         int id = item.getItemId();
@@ -179,6 +192,12 @@ public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuIte
         return false;
     }
 
+    /**
+     * Updates the ImageView with the image selected by the user. This method retrieves a bitmap image from the provided URI
+     * and sets it as the content of the ImageView designated for displaying the event's poster.
+     *
+     * @param uri The URI of the image selected by the user, typically obtained through an image picker.
+     */
     private void updateImage(Uri uri){
         //we get the bitmap from the uri that is returned by the imagePicker activity
         //we upload this bitmap to the database, and display it on-screen
@@ -197,6 +216,14 @@ public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuIte
         }
     }
 
+    /**
+     * Callback method invoked by {@link ImageDecoder} when the header of an image has been decoded, but before the entire image is decoded.
+     * This method allows for adjusting the decoding process, such as setting the target size of the decoded image.
+     *
+     * @param decoder The ImageDecoder that is decoding the image.
+     * @param info Information about the image being decoded.
+     * @param source The source from which the image is being decoded.
+     */
     @Override
     public void onHeaderDecoded(@NonNull ImageDecoder decoder, @NonNull ImageDecoder.ImageInfo info, @NonNull ImageDecoder.Source source) {
         decoder.setTargetSize(800, 400);
@@ -211,6 +238,13 @@ public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuIte
 //        return event;
 //    }
 
+    /**
+     * Generates a unique file path name for storing the event's poster image. This method constructs a path name using the event name and a timestamp
+     * to ensure uniqueness, avoiding conflicts with existing files in the storage location.
+     *
+     * @param name The name of the event, used as part of the path name to provide context.
+     * @return A unique string path name for storing the image file.
+     */
     private String generateUniquePathName(String name){
         //we generate a timestamp that contains the date and time the qr was generated. this allows us to prevent naming our qrcode as something already saved in the database
         //this idea for safe name generation is from https://developer.android.com/media/camera/camera-deprecated/photobasics accessed on Feb. 24, 2024
@@ -218,6 +252,14 @@ public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuIte
         String pathName = "poster/" + name + timeStamp;
         return pathName;
     }
+
+    /**
+     * Prepares a new Bundle with updated event creation details, including the URI of the uploaded image if available.
+     * This method is called before navigating to the next fragment in the event creation process to ensure the state is passed forward.
+     *
+     * @param bundle The current Bundle containing the event's details.
+     * @return The updated Bundle with the newly added or modified image URI and path.
+     */
     private Bundle makeNewBundle(Bundle bundle){
         Event.EventBuilder builder = (Event.EventBuilder) bundle.getSerializable("builder");
         if (this.uri == null){
@@ -245,6 +287,13 @@ public class NewEventImageFragment extends Fragment implements Toolbar.OnMenuIte
         return bundle;
     }
 
+    /**
+     * Handles the arguments passed to the fragment, specifically looking for an image URI if one has been previously selected.
+     * If an image has already been chosen during the event creation process, this method retrieves it and updates the ImageView to display it.
+     *
+     * @param args The Bundle containing the event creation arguments and potentially an image URI.
+     * @param view The root view of the fragment where the ImageView is located.
+     */
     private void handleArguments(Bundle args, View view){
         Event.EventBuilder builder = (Event.EventBuilder) args.getSerializable("builder");
         //if builder.getPosterPath != null, the user has already been to this screen so we restore their state
